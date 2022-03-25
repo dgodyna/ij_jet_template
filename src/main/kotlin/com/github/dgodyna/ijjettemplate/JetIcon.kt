cfea0dbe2867dd1eeea35992ba1c73aaa70fd041
dmgo1014 <dmitry.godyna@netcracker.com>
2022-03-25 11:31:32 +0400

    added icons

--- src/main/kotlin/com/github/dgodyna/ijjettemplate/JetIcon.kt
+++ src/main/kotlin/com/github/dgodyna/ijjettemplate/JetIcon.kt
@@ -6,6 +6,6 @@ import com.intellij.openapi.util.IconLoader
 class JetIcon {
     companion object {
         @JvmStatic
-        val FILE = IconLoader.getIcon("/icons/jet.png", JetIcon.javaClass)
+        val FILE = IconLoader.getIcon("/icons/jet_22_nb.png", JetIcon.javaClass)
     }
 }
