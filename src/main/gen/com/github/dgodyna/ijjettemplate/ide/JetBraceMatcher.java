272cc4e59d63b3a504ecc3aab52fe1cc86a09edf
dmgo1014 <dmitry.godyna@netcracker.com>
2022-03-31 11:21:03 +0400

    added brace matcher

---
+++ src/main/gen/com/github/dgodyna/ijjettemplate/ide/JetBraceMatcher.java
@@ -0,0 +1,26 @@
+package com.github.dgodyna.ijjettemplate.ide;
+
+import com.github.dgodyna.ijjettemplate.psi.JetTypes;
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class JetBraceMatcher implements PairedBraceMatcher {
+    @Override
+    public BracePair @NotNull [] getPairs() {
+        return  new BracePair[]{new BracePair(JetTypes.LDOUBLE_BRACE, JetTypes.RDOUBLE_BRACE, false)};
+    }
+
+    @Override
+    public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType lbraceType, @Nullable IElementType contextType) {
+        return false;
+    }
+
+    @Override
+    public int getCodeConstructStart(PsiFile file, int openingBraceOffset) {
+        return 0;
+    }
+}
